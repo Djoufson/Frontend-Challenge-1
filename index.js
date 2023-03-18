21f675ad9b79a68e768c58fd1a164182ffd2a5e7
Djoufson <djouflegran@gmail.com>
2023-03-17 17:32:11 -0700

    Finishing desktop views

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const hamburger = document.getElementById('span-burger')
 const navLinks = document.getElementById('nav-links')
+const backToTopBtn = document.getElementById('back-to-top')
+
+window.addEventListener('scroll', e => {
+    // When we reach a certain position at scroll, we set
+    // the back to top button to active
+})
 
 const toggleNavbar = () => {
-    // alert('toggled')
     hamburger.classList.toggle('active')
     navLinks.classList.toggle('active')
 }
